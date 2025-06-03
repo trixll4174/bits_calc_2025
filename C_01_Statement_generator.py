55c6e939771aa3f97c91f2f45cfdf152e80a4ba4
trixll4174 <trixll4174@masseyhigh.school.nz>
2025-06-04 10:52:25 +1200

    Component

---
+++ C_01_Statement_generator.py
@@ -0,0 +1,9 @@
+# functions go here
+
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# main routine goes here
+statement_generator("Instructions", "-")
+
